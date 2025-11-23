1fd8d39d5880e28b9603b8284d0f2f334c220bfd
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2025-11-23 19:18:23 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- Leetcode-Questions/215. Kth Largest Element in an Array.cpp
+++ Leetcode-Questions/215. Kth Largest Element in an Array.cpp
@@ -21,3 +21,50 @@ public:
     }
 };
 // O(nlog(k))
+
+
+class Solution {
+    int partition(vector<int>& nums, int l, int r) {
+        int P = nums[l];
+        int i = l + 1;
+        int j = r;
+
+        while (i <= j) {
+            if (nums[i] < P && nums[j] > P) {
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
+
+            if (nums[i] >= P)
+                i++;
+            if (nums[j] <= P)
+                j--; 
+        }
+        swap(nums[l], nums[j]);
+        return j;
+    }
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
+
+        int pivot = 0;
+
+        while (true) {
+            pivot = partition(nums, l, r);
+
+            if (pivot == (k - 1))
+                break;
+            if (pivot > (k - 1))
+                r = pivot - 1;
+            else
+                l = pivot + 1;
+        }
+
+        return nums[pivot];
+    }
+};
+// using quickselect algorithm
+// O(n) on average, O(n2) in the worst case
