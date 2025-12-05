955c5801d50169232b62c4e7a66e94fc43985416
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2025-12-05 22:11:11 +0530

    Update 3432. Count Partitions with Even Sum Difference.cpp

--- Leetcode-Questions/3432. Count Partitions with Even Sum Difference.cpp
+++ Leetcode-Questions/3432. Count Partitions with Even Sum Difference.cpp
@@ -23,3 +23,70 @@ public:
         return cnt;
     }
 };
+
+
+// Approach 2
+// TC = O(2n)
+// SC = O(n)
+class Solution
+{
+public:
+    int countPartitions(vector<int> &nums)
+    {
+        int cnt = 0;
+        int n = nums.size();
+        vector<long long> sufSum(n, 0);
+        for (int i = n - 1; i >= 0; i--)
+            sufSum[i] = (i < n - 1) ? sufSum[i + 1] + nums[i] : nums[i];
+
+        long long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (i < n - 1 && (abs(sum - sufSum[i + 1]) & 1) != 1)
+                cnt++;
+        }
+        return cnt;
+    }
+};
+
+// Approach 3
+// TC = O(2n)
+// Sc = O(1)
+class Solution
+{
+public:
+    int countPartitions(vector<int> &nums)
+    {
+        int cnt = 0;
+        int n = nums.size();
+        long long sum = 0;
+        for (int i : nums)
+            sum += i;
+
+        long long left = 0;
+        for (int i = 0; i < n - 1; i++) {
+            left += nums[i];
+            long long right = sum - left;
+            if ((abs(left - right) & 1) != 1)
+                cnt++;
+        }
+        return cnt;
+    }
+};
+
+// Approach 4
+// TC = O(n)
+// SC = O(1)
+class Solution
+{
+public:
+    int countPartitions(vector<int> &nums)
+    {
+        int cnt = 0;
+        int n = nums.size();
+        long long sum = 0;
+        for (int i : nums)
+            sum += i;
+        return (sum & 1) == 1 ? 0 : n - 1;
+    }
+};
