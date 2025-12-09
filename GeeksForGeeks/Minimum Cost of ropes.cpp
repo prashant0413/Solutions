b864dd977ffd5e8bc635c0adbc606b69869e2d69
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2025-12-09 21:10:04 +0530

    Update Minimum Cost of ropes.cpp

--- GeeksForGeeks/Minimum Cost of ropes.cpp
+++ GeeksForGeeks/Minimum Cost of ropes.cpp
@@ -22,3 +22,32 @@ class Solution {
         return cost;
     }
 };
+
+// Approach 2
+// TC = O(nlog(n))
+// SC = O(n)
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        if (arr.size() == 1)
+            return 0;
+            
+        priority_queue<int, vector<int>, greater<>> pq;
+        for (int &i : arr)
+            pq.push(i);
+            
+        int cost = 0;
+        while (!pq.empty()) {
+            int p = pq.top();
+            pq.pop();
+            if (pq.empty())
+                break;
+            int q = pq.top();
+            pq.pop();
+            int k = p + q;
+            cost += k;
+            pq.push(k);
+        }
+        return cost;
+    }
+};
