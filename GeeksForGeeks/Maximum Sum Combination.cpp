0050a34d332930372554df3c56dae26e61ef898f
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2025-12-11 20:40:19 +0530

    Update Maximum Sum Combination.cpp

--- GeeksForGeeks/Maximum Sum Combination.cpp
+++ GeeksForGeeks/Maximum Sum Combination.cpp
@@ -16,3 +16,37 @@ class Solution {
         return ans;
     }
 };
+
+// Approach 2
+// TC = O(nlog(n))
+// SC = O(n)
+class Solution {
+  public:
+    vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+        int n = a.size();
+        sort(a.begin(), a.end(), greater<int>());
+        sort(b.begin(), b.end(), greater<int>());
+        
+        priority_queue<tuple<int, int, int>> pq;
+        set<pair<int, int>> st;
+        
+        pq.push({a[0] + b[0], 0, 0});
+        st.insert({0, 0});
+        
+        vector<int> ans;
+        while (k-- && !pq.empty()) {
+            auto [sum, i, j] = pq.top();
+            pq.pop();
+            ans.push_back(sum);
+            if (i + 1 < n && !st.count({i + 1, j})) {
+                pq.push({a[i + 1] + b[j], i + 1, j});
+                st.insert({i + 1, j});
+            }
+            if (j + 1 < n && !st.count({i, j + 1})) {
+                pq.push({a[i] + b[j + 1], i, j + 1});
+                st.insert({i, j + 1});
+            }
+        }
+        return ans;
+    }
+};
