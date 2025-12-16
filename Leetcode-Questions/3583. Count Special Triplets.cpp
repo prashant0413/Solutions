5c88d7972d69cf89dac5c3bf8217cdf09e4e2153
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2025-12-16 19:57:47 +0530

    Create 3583. Count Special Triplets.cpp

---
+++ Leetcode-Questions/3583. Count Special Triplets.cpp
@@ -0,0 +1,25 @@
+// Approach 1
+// TC = O(2n)
+// SC = O(n)
+class Solution {
+public:
+    int M = 1e9 + 7;
+    int specialTriplets(vector<int>& nums) {
+        unordered_map<int, int> mpp_left;
+        unordered_map<int, int> mpp_right;
+
+        for (int &i : nums) {
+            mpp_right[i]++;
+        }
+
+        int result = 0;
+        for (int &i : nums) {
+            mpp_right[i]--;
+            int left = mpp_left[i * 2];
+            int right = mpp_right[i * 2];
+            result = (result + (1LL * left * right)) % M;
+            mpp_left[i]++;
+        }
+        return result;
+    }
+};
