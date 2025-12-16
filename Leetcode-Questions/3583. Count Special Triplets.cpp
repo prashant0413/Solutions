27c57078f921a3ddf78a5d33739a8082e61b6895
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2025-12-16 20:40:44 +0530

    Update 3583. Count Special Triplets.cpp

--- Leetcode-Questions/3583. Count Special Triplets.cpp
+++ Leetcode-Questions/3583. Count Special Triplets.cpp
@@ -23,3 +23,26 @@ public:
         return result;
     }
 };
+
+// Approach 2
+// TC = O(n)
+// SC = O(n)
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int specialTriplets(vector<int>& nums) {
+        unordered_map<int, int> validI;
+        unordered_map<int, int> validJ;
+
+        int result = 0;
+        for (int &i : nums) {
+            if (i % 2 == 0) {
+                result = (result + validJ[i / 2]) % mod;
+            }
+
+            validJ[i] = (validJ[i] + validI[i * 2]) % mod;
+            validI[i]++;
+        }
+        return result;
+    }
+};
