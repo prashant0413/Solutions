15c8d82bc2c4b1fb04745767cc08b8407d44cba2
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2025-12-24 19:22:02 +0530

    Update Maximum Product Subarray.cpp

--- GeeksForGeeks/Maximum Product Subarray.cpp
+++ GeeksForGeeks/Maximum Product Subarray.cpp
@@ -16,3 +16,27 @@ class Solution {
     	return maxPro;
     }
 };
+
+// Approach 2
+// TC = O(n)
+// SC = O(1)
+class Solution {
+  public:
+    int maxProduct(vector<int> &arr) {
+        int n = arr.size();
+        int pre = 1;
+        int suf = 1;
+        int maxPro = INT_MIN;
+        for (int i = 0; i < n; i++)  {
+            pre *= arr[i];
+            suf *= arr[n - 1 - i];
+            
+            maxPro = max(maxPro, max(pre, suf));
+            if (pre == 0)
+                pre = 1;
+            if (suf == 0)
+                suf = 1;
+        }
+        return maxPro;
+    }
+};
