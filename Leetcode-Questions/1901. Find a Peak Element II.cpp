3873664fcc6d2cb804bf6b7d5e5ce3bb5bdad106
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-02-14 20:39:18 +0530

    Update 1901. Find a Peak Element II.cpp

--- Leetcode-Questions/1901. Find a Peak Element II.cpp
+++ Leetcode-Questions/1901. Find a Peak Element II.cpp
@@ -1,39 +1,38 @@
-class Solution
-{
-    int findMax(vector<vector<int>> &mat, int m, int n, int row)
-    {
-        int maxi = INT_MIN;
-        int idx = -1;
-        for (int i = 0; i < n; i++)
-        {
-            if (mat[row][i] > maxi)
-            {
-                maxi = mat[row][i];
+// Approach 1
+// TC = O(nlog(m))
+// SC = O(1)
+
+class Solution {
+public:
+    int maxElement(vector<int> &arr) {
+        int idx = 0;
+        int ele = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] > ele) {
+                ele = arr[i];
                 idx = i;
             }
         }
         return idx;
     }
 
-public:
-    vector<int> findPeakGrid(vector<vector<int>> &mat)
-    {
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
         int low = 0;
         int high = m - 1;
-        while (low <= high)
-        {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            int i = findMax(mat, m, n, mid);
-            int up = (mid - 1 >= 0) ? mat[mid - 1][i] : -1;
-            int down = (mid + 1 < m) ? mat[mid + 1][i] : -1;
-            if (mat[mid][i] > up && mat[mid][i] > down)
-                return {mid, i};
-            if (mat[mid][i] < up)
+            int col = maxElement(mat[mid]);
+            int top = mid > 0 ? mat[mid - 1][col] : -1;
+            int down = mid + 1 < m ? mat[mid + 1][col] : -1;
+            if (mat[mid][col] > top && mat[mid][col] > down)
+                return {mid, col};
+            if (mat[mid][col] < top) {
                 high = mid - 1;
-            else
+            } else {
                 low = mid + 1;
+            }
         }
         return {-1, -1};
     }
