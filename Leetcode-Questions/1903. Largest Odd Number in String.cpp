ba09ad526c539aaf7037cea9d1b13874037b736c
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-02-15 19:20:23 +0530

    Update 1903. Largest Odd Number in String.cpp

--- Leetcode-Questions/1903. Largest Odd Number in String.cpp
+++ Leetcode-Questions/1903. Largest Odd Number in String.cpp
@@ -1,15 +1,13 @@
-class Solution
-{
+// Approach 1
+// TC = O(n)
+// SC = O(1)
+class Solution {
 public:
-    string largestOddNumber(string num)
-    {
+    string largestOddNumber(string num) {
         int n = num.length();
-        for (int i = n - 1; i >= 0; i--)
-        {
-            if ((num[i] - '0') % 2 != 0)
-            {
-                num.resize(i + 1);
-                return num;
+        for (int i = n - 1; i >= 0; i--) {
+            if ((int)(num[i] - '0') & 1) {
+                return num.substr(0, i + 1);
             }
         }
         return "";
