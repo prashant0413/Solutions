7df3adf2b4f4a218e2e9beb14f7a469511343202
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-02-19 15:30:32 +0530

    Update 8. String to Integer (atoi).cpp

--- Leetcode-Questions/8. String to Integer (atoi).cpp
+++ Leetcode-Questions/8. String to Integer (atoi).cpp
@@ -1,13 +1,13 @@
-class Solution
-{
-    bool isDigit(char c)
-    {
+// Approach 1
+// TC = O(n)
+// SC = O(1)
+class Solution{
+    bool isDigit(char c) {
         return c >= '0' and c <= '9';
     }
 
 public:
-    int myAtoi(string s)
-    {
+    int myAtoi(string s) {
         long long result = 0;
         int n = s.length();
         int i = 0;
@@ -18,24 +18,20 @@ public:
             i++;
 
         // 2. signedness
-        if (s[i] == '+' || s[i] == '-')
-        {
+        if (s[i] == '+' || s[i] == '-') {
             if (s[i] == '+')
                 i++;
-            else if (s[i] == '-')
-            {
+            else if (s[i] == '-') {
                 i++;
                 sign = true;
             }
         }
 
         // 3. conversion
-        while (i < n && isDigit(s[i]))
-        {
+        while (i < n && isDigit(s[i])) {
             int digit = s[i] - '0';
             result = result * 10 + digit;
-            if (result > INT_MAX)
-            {
+            if (result > INT_MAX) {
                 if (sign)
                     return INT_MIN;
                 else
