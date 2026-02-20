0cfb62db989be8ab14a7b2705232e87195e87dda
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-02-20 19:42:40 +0530

    Update 5. Longest Palindromic Substring.cpp

--- Leetcode-Questions/5. Longest Palindromic Substring.cpp
+++ Leetcode-Questions/5. Longest Palindromic Substring.cpp
@@ -1,3 +1,37 @@
+// Approach 1
+// TC = O(N^3)
+// SC = O(1)
+class Solution {
+public:
+    bool isPalindrome(string &s, int i , int j) {
+        if (i >= j)
+            return true;
+
+        if (s[i] != s[j])
+            return false;
+        return isPalindrome(s, i + 1, j - 1);
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int maxLen = INT_MIN;
+        int st = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {;
+                if (isPalindrome(s, i, j)) {
+                    if (maxLen < j - i + 1) {
+                        maxLen = j - i + 1;
+                        st = i;
+                    }
+                }
+            }
+        }
+        return s.substr(st, maxLen);
+    }
+};
+
+
+
 class Solution
 {
 public:
