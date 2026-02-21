9e793301ea49c0fc17f89dadc5dd2aae2e897bde
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-02-21 19:05:33 +0530

    Update 762. Prime Number of Set Bits in Binary Representation.cpp

--- Leetcode-Questions/762. Prime Number of Set Bits in Binary Representation.cpp
+++ Leetcode-Questions/762. Prime Number of Set Bits in Binary Representation.cpp
@@ -23,3 +23,27 @@ public:
         return cnt;
     }
 };
+
+// Approach 2
+// TC = O(right−left+1)
+// SC = O(1)
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        int cnt = 0;
+        vector<bool> isPrime(33, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i * i <= 32; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= 32; j += i) 
+                    isPrime[j] = false;
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            int x = __builtin_popcount(i);
+            if (isPrime[x])
+                cnt++;
+        }
+        return cnt;
+    }
+};
