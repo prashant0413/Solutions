02bbb782e6b2e98eee7e54875f97fa98910f70f2
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-03-11 19:34:34 +0530

    Update 1009. Complement of Base 10 Integer.cpp

--- Leetcode-Questions/1009. Complement of Base 10 Integer.cpp
+++ Leetcode-Questions/1009. Complement of Base 10 Integer.cpp
@@ -15,3 +15,33 @@ public:
         return ans;
     }
 };
+
+// Apporch 2
+// TC = O(log(n))
+// SC = O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+
+        int mask = 0;
+        while (mask < n) {
+            mask <<= 1;
+            mask |= 1;
+        }
+
+        return mask ^ n;
+    }
+};
+
+// Approach 3
+// TC = O(1)
+// SC = O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        int mask = (1 << ((int)log2(n) + 1)) - 1;
+        return n ^ mask;
+    }
+};
