9f8749cd5a4b5d6fa86a2c916f1d56af540fdcea
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-02 22:39:27 +0530

    Update 1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp

--- Leetcode-Questions/1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp
+++ Leetcode-Questions/1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp
@@ -33,3 +33,38 @@ public:
         return head;
     }
 };
+
+// Approach 2
+// TC = O(N*N)
+// SC = O(N)
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        if (head == nullptr)
+            return head;
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        unordered_map<int, ListNode*> mpp;
+        mpp[0] = dummy;
+        int preSum = 0;
+        while (head) {
+            preSum += head->val;
+            if (mpp.count(preSum)) {
+                ListNode* start = mpp[preSum];
+                ListNode* temp = start;
+                int pre = preSum;
+                while (temp != head) {
+                    temp = temp->next;
+                    pre += temp->val;
+                    if (temp != head)
+                        mpp.erase(pre);
+                }
+                start->next = head->next;
+            } else {
+                mpp[preSum] = head;
+            }
+            head = head->next;
+        }
+        return dummy->next;
+    }
+};
