886bbec318ccf66351147c9b347232dce273b551
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-03 23:00:44 +0530

    Create 102. Binary Tree Level Order Traversal.cpp

---
+++ CodeForces-Questions/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,41 @@
+// Approach 1
+// TC = O(2N)
+// SC = O(N)
+class Solution {
+public:
+    int totalNodes(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int n = 1;
+        n += totalNodes(root->left);
+        n += totalNodes(root->right);
+        return n;
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (root == nullptr) return {};
+        int n = totalNodes(root);
+        vector<vector<int>> ans(n);
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
+            pair<TreeNode*, int> p = q.front();
+            q.pop();
+            if (p.first->left) {
+                q.push({p.first->left, p.second + 1});
+            }
+            if (p.first->right) {
+                q.push({p.first->right, p.second + 1});
+            }
+
+            ans[p.second].push_back(p.first->val);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (ans[i].size() == 0)
+                ans.pop_back();
+        }
+
+        return ans;
+    }
+};
