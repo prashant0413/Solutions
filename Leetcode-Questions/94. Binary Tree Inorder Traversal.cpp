f0fb4d74b88af1fcec52d3c7af6f426d77719ebd
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-03 21:15:09 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- Leetcode-Questions/94. Binary Tree Inorder Traversal.cpp
+++ Leetcode-Questions/94. Binary Tree Inorder Traversal.cpp
@@ -1,18 +1,6 @@
 // Approach 1
 // TC = O(N)
-// SC = O(1)
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// SC = O(N)
 class Solution {
 public:
     void solve(TreeNode* root, vector<int> &ans) {
