040a6a50a123b6d26cb8273456184e4b3be0b08f
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-05 14:50:59 +0530

    Update 144. Binary Tree Preorder Traversal.cpp

--- Leetcode-Questions/144. Binary Tree Preorder Traversal.cpp
+++ Leetcode-Questions/144. Binary Tree Preorder Traversal.cpp
@@ -1,4 +1,4 @@
-// Approach 1
+// Approach 1 (Recursive)
 // TC = O(N)
 // SC = O(N)
 class Solution {
@@ -10,6 +10,35 @@ public:
         ans.push_back(root->val);
         preorderTraversal(root->left);
         preorderTraversal(root->right);
+        return ans;
+    }
+};
+
+
+// Approach 2 (Iterarive)
+// TC = O(N)
+// SC = O(N)
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int> &ans) {
+        TreeNode* node = root;
+        stack<TreeNode*> st;
+        while (1) {
+            while (node) {
+                ans.push_back(node->val);
+                st.push(node);
+                node = node->left;
+            }
+            if (st.empty())
+                break;
+            node = st.top(); st.pop();
+            node = node->right;
+        }
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
         return ans;
     }
 };
