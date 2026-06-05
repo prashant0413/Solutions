8bff04e31dc7e3449268f61a0670353ace21a2fc
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-05 15:53:24 +0530

    Update 145. Binary Tree Postorder Traversal.cpp

--- Leetcode-Questions/145. Binary Tree Postorder Traversal.cpp
+++ Leetcode-Questions/145. Binary Tree Postorder Traversal.cpp
@@ -16,3 +16,31 @@ public:
         return ans;
     }
 };
+
+// Approach 2 (Iterative) using 2 stacks
+// TC = O(N)
+// SC = O(N)
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (root == nullptr)
+            return ans;
+        stack<TreeNode*> st1;
+        stack<int> st2;
+        st1.push(root);
+        while (!st1.empty()) {
+            TreeNode* node = st1.top(); st1.pop();
+            st2.push(node->val);
+            if (node->left)
+                st1.push(node->left);
+            if (node->right)
+                st1.push(node->right);
+        }
+
+        while (!st2.empty()) {
+            ans.push_back(st2.top()); st2.pop();
+        }
+        return ans;
+    }
+};
