a57afe9b490b52e51110488337ade9201ee6af60
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-05 20:45:04 +0530

    Update 3633. Earliest Finish Time for Land and Water Rides I.cpp

--- Leetcode-Questions/3633. Earliest Finish Time for Land and Water Rides I.cpp
+++ Leetcode-Questions/3633. Earliest Finish Time for Land and Water Rides I.cpp
@@ -23,3 +23,32 @@ public:
         return result;
     }
 };
+
+// Approach 2
+// TC = O(4N)
+// SC = O(1)
+class Solution {
+public:
+    int solve(vector<int> &actTime1, vector<int> &actDuration1, vector<int> &actTime2, vector<int> &actDuration2) {
+        int m = actTime1.size();
+        int n = actTime2.size();
+        int result = INT_MAX;
+        int time1 = INT_MAX;
+        for (int i = 0; i < m; i++) {
+            time1 = min(actTime1[i] + actDuration1[i], time1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int res = max(time1, actTime2[i]) + actDuration2[i];
+            result = min(result, res);
+        }
+
+        return result;
+    }
+
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int result1 = solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int result2 = solve(waterStartTime, waterDuration, landStartTime, landDuration);
+        return min(result1, result2);
+    }
+};
