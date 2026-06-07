9f7b29de53e797d559a58a7ccfd993604cdb35b4
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-07 15:57:11 +0530

    Update 100. Same Tree.cpp

--- Leetcode-Questions/100. Same Tree.cpp
+++ Leetcode-Questions/100. Same Tree.cpp
@@ -31,3 +31,15 @@ public:
         return true;
     }
 };
+
+// Approach 2
+// TC = O(N)
+// SC = O(N)
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr || q == nullptr)
+            return p == q;
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
