b4d0d8992e8c3fd0e5bc461e7f1085cc956a5108
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-07 20:57:21 +0530

    Update 2196. Create Binary Tree From Descriptions.cpp

--- Leetcode-Questions/2196. Create Binary Tree From Descriptions.cpp
+++ Leetcode-Questions/2196. Create Binary Tree From Descriptions.cpp
@@ -42,3 +42,45 @@ public:
         return r;
     }
 };
+
+// Approach 2
+// TC = O(N)
+// SC = O(N)
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> mpp;
+        unordered_map<int, int> parent;
+        for (int i = 0; i < descriptions.size(); i++) {
+            int root = descriptions[i][0];
+            int child = descriptions[i][1];
+            int isLeft = descriptions[i][2];
+
+            TreeNode* rNode = nullptr;
+            if (mpp.count(root)) {
+                rNode = mpp[root];
+            } else {
+                rNode = new TreeNode(root);
+            }
+
+            TreeNode* cNode = nullptr;
+            if (mpp.count(child)) {
+                cNode = mpp[child];
+            } else {
+                cNode = new TreeNode(child);
+            }
+
+            if (isLeft) rNode->left = cNode;
+            else rNode->right = cNode;
+            mpp[child] = cNode;
+            mpp[root] = rNode;
+            parent[child] = root;
+        }
+
+        TreeNode* r = mpp[descriptions[0][0]];
+        while (parent.count(r->val)) {
+            r = mpp[parent[r->val]];
+        }
+        return r;
+    }
+};
