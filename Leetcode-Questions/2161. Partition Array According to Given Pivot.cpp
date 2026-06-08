b6e62b6a15a0d6deb95c907077c3efc786882b04
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-08 23:01:12 +0530

    Update 2161. Partition Array According to Given Pivot.cpp

--- Leetcode-Questions/2161. Partition Array According to Given Pivot.cpp
+++ Leetcode-Questions/2161. Partition Array According to Given Pivot.cpp
@@ -27,3 +27,33 @@ public:
         return ans;
     }
 };
+
+// Approach 2
+// TC = O(2N)
+// SC = O(1)
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int l = 0;
+        int h = n - 1;
+        int i = 0;
+        int j = n - 1;
+        while (i < n && j >= 0) {
+            if (nums[i] < pivot)
+                ans[l++] = nums[i];
+            if (nums[j] > pivot)
+                ans[h--] = nums[j];
+            i++;
+            j--;
+        }
+
+        while (l <= h) {
+            ans[l] = pivot;
+            l++;
+        }
+
+        return ans;
+    }
+};
