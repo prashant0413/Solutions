2cfb93b6dd3cdddef2e5b86acbbbbc56fe44fe3c
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-11 20:28:06 +0530

    Update 199. Binary Tree Right Side View.cpp

--- Leetcode-Questions/199. Binary Tree Right Side View.cpp
+++ Leetcode-Questions/199. Binary Tree Right Side View.cpp
@@ -1,9 +1,9 @@
-// Approach 1
+// Approach 1 using iteration
 // TC = O(N)
 // SC = O(N)
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
+    vector<int> rightSideView(TreeNode* root) { 
         vector<int> ans;
         if (root == nullptr) return ans;
         queue<TreeNode*> q;
@@ -19,6 +19,26 @@ public:
             }
             ans.push_back(x);
         }
+        return ans;
+    }
+};
+
+// Approach 2 using recursion
+// TC = O(N)
+// SC = O(H)
+class Solution {
+public:
+    void solve(TreeNode* root, int level, vector<int> &ans) {
+        if (root == nullptr) return;
+        if (level == ans.size())
+            ans.push_back(root->val);
+        solve(root->right, level + 1, ans);
+        solve(root->left, level + 1, ans);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        solve(root, 0, ans);
         return ans;
     }
 };
