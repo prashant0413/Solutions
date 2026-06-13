928d7949529fb35aa9932ed0f366a7eada4b7860
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-13 15:55:21 +0530

    Create 662. Maximum Width of Binary Tree.cpp

---
+++ Leetcode-Questions/662. Maximum Width of Binary Tree.cpp
@@ -0,0 +1,28 @@
+// Approach 1
+// TC = O(N)
+// SC = O(N)
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int ans = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
+            int size = q.size();
+            int mini = q.front().second;
+            int first, last;
+            for (int i = 0; i < size; i++) {
+                int curr = q.front().second - mini;
+                TreeNode* node = q.front().first;
+                q.pop();
+                if (i == 0) first = curr;
+                if (i == size - 1) last = curr;
+                if (node->left) q.push({node->left, (long long)curr * 2 + 1});
+                if (node->right) q.push({node->right, (long long)curr * 2 + 2});
+            }
+            ans = max(ans, last - first + 1);
+        }
+        return ans;
+    }
+};
