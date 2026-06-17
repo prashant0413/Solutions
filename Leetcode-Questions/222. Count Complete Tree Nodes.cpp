4aa523749cc7e98a55c4eb8e82859b8607d2415f
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-17 22:02:03 +0530

    Update 222. Count Complete Tree Nodes.cpp

--- Leetcode-Questions/222. Count Complete Tree Nodes.cpp
+++ Leetcode-Questions/222. Count Complete Tree Nodes.cpp
@@ -13,3 +13,41 @@ public:
         return solve(root);
     }
 };
+
+
+// Approach 2
+// TC = O((log(n)^2)
+// SC = O(log(n))
+class Solution {
+public:
+    int findLeftHeight(TreeNode* node) {
+        TreeNode* curr = node;
+        int h = 0;
+        while (curr) {
+            curr = curr->left;
+            h++;
+        }
+        return h;
+    }
+
+    int findRightHeight(TreeNode* node) {
+        TreeNode* curr = node;
+        int h = 0;
+        while (curr) {
+            curr = curr->right;
+            h++;
+        }
+        return h;
+    }
+
+
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int lh = findLeftHeight(root);   
+        int rh = findRightHeight(root);
+
+        if (lh == rh) return (1 << lh) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);   
+    }
+};
