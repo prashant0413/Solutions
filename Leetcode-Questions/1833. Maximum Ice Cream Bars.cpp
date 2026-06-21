345de0f3f0c56e0d5693f5bb4d921ddefa0e2d76
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-21 15:56:04 +0530

    Update 1833. Maximum Ice Cream Bars.cpp

--- Leetcode-Questions/1833. Maximum Ice Cream Bars.cpp
+++ Leetcode-Questions/1833. Maximum Ice Cream Bars.cpp
@@ -14,3 +14,26 @@ public:
         return cnt;
     }
 };
+
+// Approach 2
+// TC = O(n + k)
+// SC = O(k)
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int maxi = *max_element(begin(costs), end(costs));
+        vector<int> freq(maxi + 1, 0);
+        for (int &i: costs) {
+            freq[i]++;
+        }
+        int cnt = 0;
+        for (int i = 1; i <= maxi; i++) {
+            if (freq[i] == 0) continue;
+            if (coins < i) return cnt;
+            int buy = min(freq[i], coins / i);
+            cnt += buy;
+            coins -= buy * i;
+        }
+        return cnt;
+    }
+};
