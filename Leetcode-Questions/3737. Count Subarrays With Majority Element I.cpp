5e67f2cc2330fcb679825af3dd644a7371b5b200
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-06-26 15:07:05 +0530

    Update 3737. Count Subarrays With Majority Element I.cpp

--- Leetcode-Questions/3737. Count Subarrays With Majority Element I.cpp
+++ Leetcode-Questions/3737. Count Subarrays With Majority Element I.cpp
@@ -16,3 +16,23 @@ public:
         return cnt;
     }
 };
+
+// Approach 2 computational improvement
+// TC = O(N^2)
+// SC = O(1)
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int c = 0;
+            for (int j = i; j < n; j++) {
+                if (target == nums[j]) c++;
+                else c--;
+                if (c > 0) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
