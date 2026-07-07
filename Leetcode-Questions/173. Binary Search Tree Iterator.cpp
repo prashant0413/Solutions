f8389db670ff2b0e8681ab9c7e3eb7372616f94b
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-07-07 21:03:35 +0530

    Update 173. Binary Search Tree Iterator.cpp

--- Leetcode-Questions/173. Binary Search Tree Iterator.cpp
+++ Leetcode-Questions/173. Binary Search Tree Iterator.cpp
@@ -36,3 +36,32 @@ public:
         return i < inorder.size();
     }
 };
+
+// Approach 2
+// TC = O(H)
+// SC = O(H)
+class BSTIterator {
+    stack<TreeNode*> st;
+public:
+    BSTIterator(TreeNode* root) {
+        addAll(root);
+    }
+    
+    int next() {
+        TreeNode* curr = st.top();
+        st.pop();
+        addAll(curr->right);
+        return curr->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+private:
+    void addAll(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+};
