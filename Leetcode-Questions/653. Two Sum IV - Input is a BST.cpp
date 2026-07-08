6a49be5ab0c0aee10306d2fca5c2b2dff67caee4
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-07-08 20:26:37 +0530

    Update 653. Two Sum IV - Input is a BST.cpp

--- Leetcode-Questions/653. Two Sum IV - Input is a BST.cpp
+++ Leetcode-Questions/653. Two Sum IV - Input is a BST.cpp
@@ -43,3 +43,53 @@ public:
         return false;
     }
 };
+
+// Approach 1
+// TC = O(N)
+// SC = O(1)
+class BSTIterator {
+    stack<TreeNode*> st;
+    bool isBefore;
+public:
+    BSTIterator(TreeNode* root, bool before) {
+        isBefore = before;
+        addAll(root);
+    }
+
+    int next() {
+        TreeNode* curr = st.top();
+        st.pop();
+        if (!isBefore) addAll(curr->right);
+        else addAll(curr->left);
+        return curr->val;
+    }
+
+private:
+    void addAll(TreeNode* root) {
+        while (root) {
+            st.push(root);
+            if (isBefore) {
+                root = root->right;
+            } else {
+                root = root->left;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+        BSTIterator low(root, false);
+        BSTIterator high(root, true);
+        int i = low.next();
+        int j = high.next();
+        while (i < j) {
+            if (i + j == k) return true;
+            if (i + j < k) i = low.next();
+            else j = high.next();
+        }
+        return false;
+    }
+};
