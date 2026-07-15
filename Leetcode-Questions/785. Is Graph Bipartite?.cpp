e8783ffe889c81b63a02a7e0d9fb50a446c49b0c
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-07-15 22:42:38 +0530

    Update 785. Is Graph Bipartite?.cpp

--- Leetcode-Questions/785. Is Graph Bipartite?.cpp
+++ Leetcode-Questions/785. Is Graph Bipartite?.cpp
@@ -1,4 +1,4 @@
-// Approach 1
+// Approach 1 using DFS
 // TC = O(V + 2E)
 // SC = O(V)
 class Solution {
@@ -25,6 +25,40 @@ public:
             if (color[i] == -1) {
                 if (!dfs(i, 0, color, graph)) {
                     return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+// Approach 2 using BFS
+// TC = O(V + 2E)
+// SC = O(V)
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        int color[V];
+        for (int i = 0; i < V; i++) color[i] = -1;
+
+        queue<int> q;
+        for (int v = 0; v < V; v++) {
+            if (color[v] == -1) {
+                q.push(v);
+                color[v] = 0;
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    for (int &i: graph[node]) {
+                        if (color[i] == -1) {
+                            color[i] = 1 - color[node];
+                            q.push(i);
+                        } else {
+                            if (color[i] == color[node]) return false;
+                        }
+                    }
                 }
             }
         }
