a39c7991fc3ce25c72b114fcf1eaa191b07da9a2
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-07-20 15:05:55 +0530

    Update Dijkstra Algorithm.cpp

--- GeeksForGeeks/Dijkstra Algorithm.cpp
+++ GeeksForGeeks/Dijkstra Algorithm.cpp
@@ -1,4 +1,4 @@
-// Approach 1
+// Approach 1 using priority queue
 // TC = O((V + E)LOG(V))
 // SC = O(V)
 class Solution {
@@ -29,6 +29,53 @@ class Solution {
                 if (d + w < dist[v]) {
                     dist[v] = d + w;
                     pq.push({dist[v], v});
+                }
+            }
+        }
+        
+        return dist;
+    }
+};
+
+// Approach 2 using set
+// TC = O((V + E)LOG(V))
+// SC = O(V)
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<pair<int, int>> adj[V];
+        for (auto it: edges) {
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+        
+        vector<int> dist(V, 1e9);
+        dist[src] = 0;
+        set<pair<int, int>> st;
+        st.insert({0, src});
+        while (!st.empty()) {
+            auto it = *(st.begin());
+            int node = it.second;
+            int w = it.first;
+            st.erase(it);
+            
+            for (auto i: adj[node]) {
+                int v = i.first;
+                int wt = i.second;
+                
+                if (dist[v] == 1e9) {
+                    dist[v] = w + wt;
+                    st.insert({dist[v], v});
+                } else {
+                    if (wt + w < dist[v]) {
+                        st.erase({dist[v], v});
+                        dist[v] = wt + w;
+                        st.insert({dist[v], v});
+                    }
                 }
             }
         }
