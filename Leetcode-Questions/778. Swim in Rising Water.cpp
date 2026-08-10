559cdf0ffb5c794fc98377abb15a5213a5ddd6ce
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-08-10 22:37:00 +0530

    Update 778. Swim in Rising Water.cpp

--- Leetcode-Questions/778. Swim in Rising Water.cpp
+++ Leetcode-Questions/778. Swim in Rising Water.cpp
@@ -1,4 +1,4 @@
-// Approach 1
+// Approach 1 using Binary Search on answers
 // TC = O(n^2 * log(n))
 // SC = O(n^2)
 class Solution {
@@ -35,5 +35,37 @@ public:
             }
         }
         return result;
+    }
+};
+
+// Approach 2 using Shortest path
+// TC = O(n^2 * log(n))
+// SC = O(n^2)
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
+        pq.push({grid[0][0], {0, 0}});
+        vis[0][0] = true;
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        while (!pq.empty()) {
+            auto it = pq.top(); pq.pop();
+            int t = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+            if (r == n -1 && c == n -1) return t;
+            for (int i = 0; i < 4; i++) {
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !vis[nr][nc]) {
+                    vis[nr][nc] = true;
+                    pq.push({max(t, grid[nr][nc]), {nr, nc}});
+                }
+            }
+        } 
+        return 0;
     }
 };
