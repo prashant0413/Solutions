937f4700b34df276445ab582478deb5d266e915c
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-08-20 15:33:34 +0530

    Update 1386. Cinema Seat Allocation.cpp

--- Leetcode-Questions/1386. Cinema Seat Allocation.cpp
+++ Leetcode-Questions/1386. Cinema Seat Allocation.cpp
@@ -28,3 +28,33 @@ public:
         return result;
     }
 };
+
+// Approach 2
+// TC = O(reservedSeats)
+// SC = O(1)
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int, int> mpp;
+        for (auto it: reservedSeats) {
+            mpp[it[0]] |= (1 << it[1]);
+        }
+
+        int result = (n - mpp.size()) * 2;
+        int groupAMask = 0b111100;
+        int groupBMask = 0b11110000;
+        int groupCMask = 0b1111000000;
+
+        for (auto& [row, seats]: mpp) {
+            bool groupA = (seats & groupAMask) == 0;
+            bool groupB = (seats & groupBMask) == 0;
+            bool groupC = (seats & groupCMask) == 0;
+
+            if (groupA && groupC)
+                result += 2;
+            else if (groupA || groupB || groupC)
+                result++;
+        }
+        return result;
+    }
+};
