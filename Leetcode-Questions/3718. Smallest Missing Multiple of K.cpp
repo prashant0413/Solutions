95f8445cf3df287fa48698f50ca124ba93968a2a
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-08-25 09:42:53 +0530

    Update 3718. Smallest Missing Multiple of K.cpp

--- Leetcode-Questions/3718. Smallest Missing Multiple of K.cpp
+++ Leetcode-Questions/3718. Smallest Missing Multiple of K.cpp
@@ -1,4 +1,27 @@
-// Approach 1
+// Approach 1 brute force
+// TC = O(N^2)
+// SC = O(1)
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i = 1;
+        for (; ; i++) {
+            bool pre = false;
+            for (int e: nums) {
+                if (k * i == e) {
+                    pre = true;
+                    break;
+                }
+            }
+            if (!pre)
+                return i * k;
+        }        
+        return 0;
+    }
+};
+
+// Approach 2 using set
 // TC = O(N)
 // SC = O(N)
 class Solution {
