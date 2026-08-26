921bf5368ec168aac738ce04535ac320f49c7f41
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-08-26 21:33:19 +0530

    Update 2904. Shortest and Lexicographically Smallest Beautiful String.cpp

--- Leetcode-Questions/2904. Shortest and Lexicographically Smallest Beautiful String.cpp
+++ Leetcode-Questions/2904. Shortest and Lexicographically Smallest Beautiful String.cpp
@@ -32,3 +32,37 @@ public:
         return "";
     }
 };
+
+// Approach 2 using sliding window
+// TC = O(n^2)
+// SC = O(n)
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.length();
+        int l, r;
+        l = r = 0;
+        int ones = 0;
+        string result = "";
+        while (r < n) {
+            if (s[r] == '1') {
+                ones++;
+            }
+
+            while (ones > k || s[l] == '0') {
+                if (s[l] == '1')
+                    ones--;
+                l++;
+            }
+
+            if (ones == k) {
+                string temp = s.substr(l, r - l + 1);
+                if (result.empty() || temp.length() < result.length() || (temp.length() == result.length() && temp < result)) {
+                    result = temp;
+                }
+            }
+            r++;
+        }
+        return result;
+    }
+};
