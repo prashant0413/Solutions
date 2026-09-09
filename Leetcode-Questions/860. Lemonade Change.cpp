70e8e092d1091428de2de293ce53dc875a2ae916
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-09 22:55:52 +0530

    Create 860. Lemonade Change.cpp

---
+++ Leetcode-Questions/860. Lemonade Change.cpp
@@ -0,0 +1,31 @@
+// Approach 1
+// TC = O(N)
+// SC = O(1)
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int cnt5 = 0;
+        int cnt10 = 0;
+        for (int i = 0; i < bills.size(); i++) {
+            if (bills[i] == 5) cnt5++;
+            else if (bills[i] == 10) {
+                if (cnt5 > 0) {
+                    cnt5--;
+                    cnt10++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (cnt10 > 0 && cnt5 > 0) {
+                    cnt10--;
+                    cnt5--;
+                } else if (cnt5 > 2) {
+                    cnt5 -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
