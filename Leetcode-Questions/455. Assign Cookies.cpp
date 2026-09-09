a835c81c2be8985fdd92124b00db8d941bd76c4d
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-09 21:52:09 +0530

    Update 455. Assign Cookies.cpp

--- Leetcode-Questions/455. Assign Cookies.cpp
+++ Leetcode-Questions/455. Assign Cookies.cpp
@@ -4,22 +4,26 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int cnt = 0;
-        sort(begin(g), end(g));
-        sort(begin(s), end(s));
         int n1 = g.size();
         int n2 = s.size();
-        int i, j;
-        i = j = 0;
+
+        sort(begin(g), end(g));
+        sort(begin(s), end(s));
+
+        int cnt = 0;
+        int i = 0;
+        int j = 0;
+
         while (i < n1 && j < n2) {
             if (g[i] <= s[j]) {
                 cnt++;
                 i++;
                 j++;
-            } else {
+            } else if (s[j] < g[i]) {
                 j++;
             }
         }
+
         return cnt;
     }
 };
