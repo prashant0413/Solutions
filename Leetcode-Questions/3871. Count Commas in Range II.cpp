c7b45dbe2c030524fb5d3ea0ff911d294d441164
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-09 20:24:32 +0530

    Create 3871. Count Commas in Range II.cpp

---
+++ Leetcode-Questions/3871. Count Commas in Range II.cpp
@@ -0,0 +1,36 @@
+// Approach 1
+// TC = O(1)
+// SC = O(1)
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if (n < 1000) return 0;
+        long long cnt = 0;
+        if (n >= (long long)1e15) {
+            long long digits = n - (long long)1e15 + 1;
+            cnt += digits * 5;
+            n -= digits;
+        } 
+        if (n >= 1e12) {
+            long long digits = n - (long long)1e12 + 1;
+            cnt += digits * 4;
+            n -= digits;
+        }
+        if (n >= 1e9) {
+            long long digits = n - (long long)1e9 + 1;
+            cnt += digits * 3;
+            n -= digits;
+        }
+        if (n >= 1e6) {
+            long long digits = n - (long long)1e6 + 1;
+            cnt += digits * 2;
+            n -= digits;
+        }
+        if (n >= 1000) {
+            long long digits = n - (long long)1e3 + 1;
+            cnt += digits * 1;
+            n -= digits;
+        }
+        return cnt;
+    }
+};
