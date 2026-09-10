30a57632d2254baa13aaea93ef7f514a92cbb45f
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-10 21:19:15 +0530

    Update 678. Valid Parenthesis String.cpp

--- Leetcode-Questions/678. Valid Parenthesis String.cpp
+++ Leetcode-Questions/678. Valid Parenthesis String.cpp
@@ -24,3 +24,29 @@ public:
         return solve(s, 0, 0);
     }
 };
+
+// Approach 2
+// TC = O(N)
+// SC = O(1)
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini = 0;
+        int maxi = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                mini += 1;
+                maxi += 1;
+            } else if (s[i] == ')') {
+                mini -= 1;
+                maxi -= 1;
+            } else {
+                mini -= 1;
+                maxi += 1;
+            }
+            if (mini < 0) mini = 0;
+            if (maxi < 0) return false;
+        }
+        return mini == 0;
+    }
+};
