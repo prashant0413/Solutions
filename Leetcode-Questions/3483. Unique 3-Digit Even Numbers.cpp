d7016fa7c62ee682ace01257fd558b1201a91ef6
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-11 15:28:03 +0530

    Update 3483. Unique 3-Digit Even Numbers.cpp

--- Leetcode-Questions/3483. Unique 3-Digit Even Numbers.cpp
+++ Leetcode-Questions/3483. Unique 3-Digit Even Numbers.cpp
@@ -24,3 +24,34 @@ public:
         return st.size();
     }
 };
+
+// Approach 2
+// TC = O(N)
+// SC = O(1)
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int freq[10] = {0};
+        for (int i: digits) {
+            freq[i]++;
+        }
+
+        int cnt = 0;
+        for (int h = 1; h <= 9; h++) {
+            if (freq[h] == 0) continue;
+            freq[h]--;
+            for (int t = 0; t <= 9; t++) {
+                if (freq[t] == 0) continue;
+                freq[t]--;
+                for (int o = 0; o <= 8; o += 2) {
+                    if (freq[o] == 0) continue;
+                    cnt++;
+                }
+                freq[t]++;
+            }
+            freq[h]++;
+        }
+
+        return cnt;
+    }
+};
