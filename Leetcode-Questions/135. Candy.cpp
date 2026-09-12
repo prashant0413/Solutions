656f15c5d6c0853056e11e21a48d411b4bbf750c
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-12 16:27:23 +0530

    Update 135. Candy.cpp

--- Leetcode-Questions/135. Candy.cpp
+++ Leetcode-Questions/135. Candy.cpp
@@ -33,3 +33,39 @@ public:
         return sum;
     }
 };
+
+// Approach 2
+// TC: O(2n)
+// SC: O(n)
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n, 0);
+        left[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+
+        int sum = max(left[n - 1], 1);
+        int curr = 1;
+        int right = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                curr = right + 1;
+                right = curr;
+            } else {
+                curr = 1;
+                right = curr;
+            }
+            sum += max(left[i], curr);
+        }
+
+        return sum;
+    }
+};
