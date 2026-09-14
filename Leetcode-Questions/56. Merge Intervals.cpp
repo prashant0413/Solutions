469c84261eb050fb816e58dbf6b70ebf5d59993a
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-14 21:27:01 +0530

    Update and rename Merge overlapping Intervals(leetcode 56).cpp to 56. Merge Intervals.cpp

---
+++ Leetcode-Questions/56. Merge Intervals.cpp
@@ -0,0 +1,26 @@
+// Approach 1
+// TC = O(N log N)
+// SC = O(N)
+class Solution {
+public:
+    struct cmp {
+        bool operator()(vector<int> &a, vector<int> &b) {
+            return a[0] < b[0];
+        }
+    };
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(begin(intervals), end(intervals), cmp());
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        for (int i = 1; i < n; i++) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
