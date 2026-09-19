a8cabda0e4b3fee9b6ef00f61538624e241fdf89
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-19 15:18:30 +0530

    Update 1823. Find the Winner of the Circular Game.cpp

--- Leetcode-Questions/1823. Find the Winner of the Circular Game.cpp
+++ Leetcode-Questions/1823. Find the Winner of the Circular Game.cpp
@@ -89,3 +89,27 @@ public:
         return l.head->val;
     }
 };
+
+// APPROACH 3: USING QUEUE
+// TC: O(N . K)
+// SC: O(N)
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> q;
+        for (int i = 1; i <= n; i++)
+            q.push(i);
+
+        while (q.size() > 1) {
+            for (int i = 1; i <= k - 1; i++) {
+                int x = q.front();
+                q.pop();
+                q.push(x);
+            }
+
+            q.pop();
+        }
+
+        return q.front();
+    }
+};
