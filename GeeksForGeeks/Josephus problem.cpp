edea8611855b21416a690739ca75e973594af918
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-19 15:35:42 +0530

    Update Josephus problem.cpp

--- GeeksForGeeks/Josephus problem.cpp
+++ GeeksForGeeks/Josephus problem.cpp
@@ -113,3 +113,18 @@ public:
         return q.front();
     }
 };
+
+// APPROACH 4: USING RECURSION
+// TC: O(N)
+// SC: O(N)
+class Solution {
+public:
+    int solve(int n, int k) {
+        if (n == 1) return 0;
+        return (solve(n - 1, k) + k) % n;
+    }
+
+    int josephus(int n, int k) {
+        return solve(n, k) + 1; 
+    }
+};
