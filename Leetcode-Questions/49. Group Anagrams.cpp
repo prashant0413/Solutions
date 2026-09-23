041b834ab7065b239fbb0ca784321f35d9aa6096
Prashant Vishwakarma <prashantvishwakarma0413@gmail.com>
2026-09-23 22:37:39 +0530

    Create 49. Group Anagrams.cpp

---
+++ Leetcode-Questions/49. Group Anagrams.cpp
@@ -0,0 +1,45 @@
+// APPROACH 1
+// TC: O(N^2 . K)
+// SC: O(N)
+class Solution {
+public:
+    bool checkAnagram(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if (n != m) return false;
+
+        int freq[26] = {0};
+        for (int i = 0; i < n; i++) {
+            freq[s1[i] - 'a']++;
+            freq[s2[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++)
+            if (freq[i] != 0) return false;
+
+        return true;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        vector<vector<string>> res;
+
+        for (int i = 0; i < n; i++) {
+            bool found = false;
+            for (int k = 0; k < res.size(); k++) {
+                if (checkAnagram(strs[i], res[k][0])) {
+                    res[k].push_back(strs[i]);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                res.push_back({strs[i]});
+            }
+        }
+
+        return res;
+    }
+};
